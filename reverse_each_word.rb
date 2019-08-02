42f6deb369f2244524006a8a10d5ab86aba40575
Kenny Marks <kmarks2013@gmail.com>
2019-08-02 01:17:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,7 +14,7 @@ def reverse_each_word(string)
   array = [string.split(" ")]
   reversed_array =[]
   
-  array.each do |n|
+  array.collect do |n|
     reversed_array.push(n.reverse)
   end
   return reversed_array.join(" ")
